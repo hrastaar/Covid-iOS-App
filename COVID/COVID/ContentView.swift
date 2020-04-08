073b4c305d96fd16b7d6bbe8a258556c1e3d0f4c
Rastaar Haghi <rhaghi@usc.edu>
2020-04-08 03:59:48 -0700

    created CovidData class + API call

--- COVID/COVID/ContentView.swift
+++ COVID/COVID/ContentView.swift
@@ -9,8 +9,14 @@
 import SwiftUI
 
 struct ContentView: View {
+    var tester = CovidData();
+    
+
     var body: some View {
-        Text("Hello, World!")
+        VStack {
+            Text("Hello, World!")
+        }
+        
     }
 }
 
