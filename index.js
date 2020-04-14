73fb6d6ae4ad5cb061d72f7519924cd1ee5fee77
Malachi Schrager <mmschrag@usc.edu>
2020-04-13 21:19:52 -0700

    test

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 
 express()
+  console.log("hi")
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
