083f3741c7ba2ebc7c4353c2fe7f699951a190b4
Malachi Schrager <mmschrag@usc.edu>
2020-04-13 21:25:02 -0700

    heroku

--- index.js
+++ index.js
@@ -9,6 +9,9 @@ const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
+  .set('views', path.join(__dirname, 'views'))
+  .set('view engine', 'ejs')
+  .get('/', (req, res) => res.render('pages/index'))
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -18,7 +21,7 @@ express()
       client.release();
     } catch (err) {
       console.error(err);
-      res.send("Error " + err);
+      res.send("Specific Error " + err);
     }
   })
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
