70e1d3eb8f831ed35229bcab696b05de4bf69fe4
Malachi Schrager <mmschrag@usc.edu>
2020-04-15 00:16:30 -0700

    podfile update

--- COVID/COVID/AppDelegate.swift
+++ COVID/COVID/AppDelegate.swift
@@ -86,10 +86,10 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication,
-      didFinishLaunchingWithOptions launchOptions:
-        [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
-      FirebaseApp.configure()
-      return true
+        didFinishLaunchingWithOptions launchOptions:
+            [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        FirebaseApp.configure()
+        return true
     }
 
 }
